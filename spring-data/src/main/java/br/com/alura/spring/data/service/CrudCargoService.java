af4e58c3772dd0d67abe099ee5b099c2cae6577d
Jefferson <jeffrm@gmail.com>
2021-09-21 23:19:53 -0300

    23-Operações CRUD - Atualizando registro

--- spring-data/src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
+++ spring-data/src/main/java/br/com/alura/spring/data/service/CrudCargoService.java
@@ -10,6 +10,8 @@ import br.com.alura.spring.data.repository.CargoRepository;
 @Service //outras classes vao depender dela
 public class CrudCargoService {
 
+	private Boolean system = true;
+	
 	public CrudCargoService(CargoRepository repository) {
 		
 		this.repository = repository;
@@ -18,6 +20,29 @@ public class CrudCargoService {
 	private final CargoRepository repository ; 
 	
 	public void inicial(Scanner scanner) {
+		
+		while(system) {
+			System.out.println("qual acao de cargo deseja executar?");
+			System.out.println("0-sair");
+			System.out.println("1-salvar");
+			System.out.println("2-atualizar");
+			
+			int action = scanner.nextInt();
+			
+			switch(action) {
+			
+			case 1: salvar(scanner);break;
+			case 2: atualizar(scanner); break;
+			default:
+					system = false;
+					break;
+				
+			
+			}
+			
+			
+		}
+		
 		salvar(scanner);
 	}
 	
@@ -33,6 +58,21 @@ public class CrudCargoService {
 		
 		
 		
+	}
+	
+	private void atualizar(Scanner scanner) {
+		System.out.println("Id");
+		Integer id = scanner.nextInt();
+		
+		System.out.println("Descricao do cargo");		
+		String descricao = scanner.next();
+		
+		Cargo cargo = new Cargo();
+		cargo.setId(id);		
+		cargo.setDescricao(descricao);
+		
+		repository.save(cargo); //atualiza caso informado id
+		System.out.println("Atualizado");
 	}
 	
 	
